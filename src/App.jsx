97e34523f27484ef6b57be59b1fb3b3f928ee773
nairx <nairxprv@gmail.com>
2026-08-31 15:55:49 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,53 @@
 import React from "react";
-
-function Child1() {
-  return <div>Child 1</div>;
-}
-
-function Child2() {
-  return <div>Child 2</div>;
-}
-
-function Home({children}) {
-  return <div style={{ color: "blue" }}>{children}</div>;
-}
+import "./App.css"
 
 export default function App() {
   return (
-    <Home>
-      <Child1 />
-      <Child2 />
-    </Home>
+    <div>
+      <h1 className="header">
+        My Application
+      </h1>
+    </div>
   );
 }
+
+// import React from "react";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1 style={{ color: "blue", backgroundColor: "Orange" }}>
+//         My Application
+//       </h1>
+//     </div>
+//   );
+// }
+
+
+
+
+// import React from "react";
+
+// function Child1() {
+//   return <div>Child 1</div>;
+// }
+
+// function Child2() {
+//   return <div>Child 2</div>;
+// }
+
+// function Home({children}) {
+//   return <div style={{ color: "blue" }}>{children}</div>;
+// }
+
+// export default function App() {
+//   return (
+//     <Home>
+//       <Child1 />
+//       <Child2 />
+//     </Home>
+//   );
+// }
 
 // function Greet({name,age}) {
 //   return <h2>Hello {name}-{age}</h2>;
