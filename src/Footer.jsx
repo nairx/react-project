2d9d14c1cc0ad47148c851ca0374eeca7e5b6634
nairx <nairxprv@gmail.com>
2026-08-31 14:55:06 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return <h4>&copy; Copyright 2026. All rights reserved.</h4>
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+function Header(){
+    return <h1>My Application</h1>
+}
+
+export default Header
