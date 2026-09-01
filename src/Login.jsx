b9fa9c8b34b4cd7a84ead26e6702839caff53858
nairx <nairxprv@gmail.com>
2026-09-01 16:14:52 +0530

    updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { AppContext } from "./App";
+import { useContext, useState } from "react";
 export default function Login() {
+  const { users, setUsers } = useContext(AppContext);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
+  const handleLogin = () => {
+    const found = users.find(
+      (user) => user.email === email && user.password === password,
+    );
+    if (found) {
+      setMessage("Welcome " + found.name);
+    } else {
+      setMessage("Access Denied");
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div>
+      Login
+      <p>{message}</p>
+      <p>
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </p>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 }
