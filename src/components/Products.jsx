861de98061cd27579abe8b011f15c9b5d97c6537
nairx <nairxprv@gmail.com>
2026-09-03 15:15:43 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Products() {
+  const [products, setProducts] = useState();
+  const [product, setProduct] = useState({});
+  const API = import.meta.env.VITE_API_URL;
+  const fetchProducts = async () => {
+    const res = await axios.get(`${API}/products`);
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleAdd = async () => {
+    const res = await axios.post(`${API}/products`, product);
+    fetchProducts();
+  };
   return (
-    <div>Products</div>
-  )
+    <div>
+      Products
+      <p>
+        <input
+          type="number"
+          placeholder="Id"
+          onChange={(e) => setProduct({ ...product, id: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setProduct({ ...product, name: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          onChange={(e) =>
+            setProduct({ ...product, description: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          onChange={(e) => setProduct({ ...product, price: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Image URL"
+          onChange={(e) => setProduct({ ...product, imageUrl: e.target.value })}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </p>
+      <ol>
+        {products &&
+          products.map((product) => (
+            <li key={product.id}>
+              {product.name}-{product.description}-{product.price}
+            </li>
+          ))}
+      </ol>
+    </div>
+  );
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Users() {
+  const [users, setUsers] = useState();
+  const API = import.meta.env.VITE_API_URL;
+  const fetchUsers = async () => {
+    const res = await axios.get(`${API}/users`);
+    setUsers(res.data);
+  };
+  useEffect(()=>{
+    fetchUsers()
+  },[])
   return (
-    <div>Users</div>
-  )
+    <div>
+      Users
+      <ol>
+        {users &&
+          users.map((user) => (
+            <li key={user.id}>
+              {user.name}-{user.email}-{user.role}
+            </li>
+          ))}
+      </ol>
+    </div>
+  );
 }
