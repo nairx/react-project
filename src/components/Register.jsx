287166ead33e7816f9eb47ae9014ddc18b3e4224
nairx <nairxprv@gmail.com>
2026-09-03 17:46:33 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,52 +2,53 @@ import React, { useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function Register() {
   const { users, setUsers } = useContext(AppContext);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const navigate = useNavigate()
-  const handleSubmit = () => {
-    setUsers([
-      ...users,
-      {
-        name,
-        email,
-        password,
-      },
-    ]);
-    navigate("/login")
+  const navigate = useNavigate();
+  const API = import.meta.env.VITE_API_URL;
+  const handleSubmit = async () => {
+    const res = await axios.post(`${API}/users/register`, { name, email, password });
+    // setUsers([
+    //   ...users,
+    //   {
+    //     name,
+    //     email,
+    //     password,
+    //   },
+    // ]);
+    navigate("/login");
   };
   return (
- 
-      <div>
-        Register
-        <p>
-          <input
-            type="text"
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </p>
-        <p>
-          <input
-            type="text"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </p>
-        <p>
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </p>
-        <p>
-          <button onClick={handleSubmit}>Submit</button>
-        </p>
-      </div>
-    
+    <div>
+      Register
+      <p>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </p>
+      <p>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
+    </div>
   );
 }
